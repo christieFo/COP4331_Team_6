7d218dcb023ca98c5aeadadb177bf893c5f8868e
SKeppinger <47565463+SKeppinger@users.noreply.github.com>
2023-11-07 20:35:39 -0500

    Add files via upload

---
+++ frontend/src/components/HomeButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function homeButton()
+{
+    return(
+        <button>Home</button>
+    )
+}
+
+export default homeButton;
---
+++ frontend/src/components/PageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function PageTitle()
+{
+  return(
+    <h1 id="title">Group 6 Fantasy Blog! (Working Title)</h1>
+  );
+}
+
+export default PageTitle;

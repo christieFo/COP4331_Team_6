3b6e82797710b690ae520305603d66fd3e136185
christieFo <fossenierce@gmail.com>
2023-11-21 18:21:31 -0500

    Late Commit
    work done by everyone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import './App.css';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import SignUpSuccessPage from './pages/SignUpSuccessPage';
+import SearchPage from './pages/SearchPage';
 
 function App()
 {
   return(
+   
     <BrowserRouter>
+    
     <Routes>
       <Route path="/" index element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/signupsuccess" element={<SignUpSuccessPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/create" element={<CreatePage />} />
+      <Route path="/search" element={<SearchPage/>}/>
+      
     </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- server.js
+++ server.js
@@ -25,13 +25,35 @@ res.setHeader(
 next();
 });
 
+
+app.post('/api/register', async (req, res, next) =>
+{
+  var error = '';
+  const { email, user, pass, confirm } = req.body;
+  
+  const db = client.db("test");
+  const emailCheck = await db.collection('users').find({email:email}).toArray();
+  if (emailCheck.length > 0)
+    res.status(201).json({error:'Email already in use'});
+  else
+  {
+    const userCheck = await db.collection('users').find({user:user}).toArray();
+    if (userCheck.length > 0)
+      res.status(202).json({error:'Username already in use'});
+    else
+    {
+      await db.collection('users').insertOne({"email":email, "user":user, "pass":pass, "userID":0});
+      res.status(200).json({error:''});
+    }
+  }
+});
 app.post('/api/login', async (req, res, next) =>
 {
   var error = '';
   const { email, password } = req.body;
 
   const db = client.db("test");
-  const results = await db.collection('users').find({email:email,password:password}).toArray();
+  const results = await db.collection('users').find({email:email.toLowerCase(),pass:password}).toArray();
 
   var id = -1;
   var un = '';
@@ -39,12 +61,51 @@ app.post('/api/login', async (req, res, next) =>
   if (results.length > 0)
   {
     id = results[0].userID;
-    un = results[0].username;
+    un = results[0].user;
   }
 
   var ret = { id:id, username:un, error:''};
   res.status(200).json(ret);
 
+});
+app.post('/api/post', async (req, res, next)=>{
+  var error = '';
+  const {title, content, author } = req.body;
+  const db = client.db("test");
+  const result = await db.collection('posts').insertOne({ title, content, author});
+  
+  var ret = {title:title, error:''};
+  res.status(200).json(ret);
+});
+app.put('/api/edit', async (req, res, next) => {
+  const postId = req.params.id;
+  const { title, content, author } = req.body;
+
+  const db = client.db("test");
+  const result = await db.collection('posts').updateOne(
+    { _id: ObjectId(postId) },
+    { $set: { title, content, author } }
+  );
+
+  if (result.modifiedCount > 0) {
+    res.status(200).json({ error: '', message: 'Post updated successfully' });
+  } else {
+    res.status(404).json({ error: 'Post not found' });
+  }
+});
+
+// Delete post
+app.delete('/api/delete', async (req, res, next) => {
+  const postId = req.params.id;
+
+  const db = client.db("test");
+  const result = await db.collection('posts').deleteOne({ _id: ObjectId(postId) });
+
+  if (result.deletedCount > 0) {
+    res.status(200).json({ error: '', message: 'Post deleted successfully' });
+  } else {
+    res.status(404).json({ error: 'Post not found' });
+  }
 });
 
 app.listen(5000); // start Node + Express server on port 5000
